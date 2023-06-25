6df45dc7bcd2881499794e2eabb1fadadaf0b59d
이규훈 <wxyz0904@naver.com>
2023-06-25 13:45:22 +0900

    Feat: 기본설정
    Gradle - Groovy
    Java 11
    Jar 
    Spring Boot 2.7.13 설정
    Dependencies: Lombok, Spring BootDevTools, Spring Web, Thmeleaf

---
+++ baas/src/main/java/com/flutter/baas/BaaSApplication.java
@@ -0,0 +1,13 @@
+package com.flutter.baas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaaSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaaSApplication.class, args);
+	}
+
+}
